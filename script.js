d1b21a43713e83f918ddbd7862df9441f3eac98b
bsamu <89023134+bsamu@users.noreply.github.com>
2021-12-02 12:43:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const selectElement = (type, id, label, selectOptions) => {
     let optionElements = "";
     for (const option of selectOptions) {
@@ -47,27 +45,92 @@ const formElement = `
 
 const sheetElement = `
     <p>Hello 
-    <span id="inputValueContent"></span>
+    <span id="inputValueContent"></span>!
     <br>
     Your gender is:
     <span id="inputValueContentGender">Female</span>
-    <img id="gImg" src="" alt=" " width="60" height="60"> 
-    !</p>
+    <img id="gImg" src="https://lexiq.hu/kepek/female.png" alt=" " width="60" height="60">
+    <br>
+    Your race:
+    <span id="inputValueContentRace">Dhampir</span>
+    <img id="rImg" src="https://images.squarespace-cdn.com/content/v1/5e3843beb37a0716cf512998/1618937697187-4SDCGSOWQK354XIYBO9P/dhampir.png" alt=" " width="60" height="60">
+    <br>
+    Your class:
+    <span id="inputValueContentClass">Fighter</span>
+    <img id="cImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgjlR5nVllPfQUR3M_AFT0x0JC02jPSG0Bhg&usqp=CAU" alt=" " width="60" height="60">
+    </p>
 `;
 
 const inputEvent = (event) => {
     document.getElementById("inputValueContent").innerHTML = event.target.value;
 }
 
+const dhampirUrl = "";
+const dragonbornUrl = "";
+const dwarfUrl = "";
+const elfUrl = "";
+
+const fighterUrl = "";
+const programmerUrl = "";
+const striderUrl = "";
+const wizardUrl = "";
+
 const selectEvent = (event) => {
-    document.getElementById("inputValueContentGender").innerHTML = event.target.value;
-    if (event.target.value === "Female") {
-        // console.log("I work");
-        document.getElementById("gImg").src = "https://lexiq.hu/kepek/female.png";
-    } else if (event.target.value === "Male") {
-        document.getElementById("gImg").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Male-s%C3%ADmbolo2.svg/1200px-Male-s%C3%ADmbolo2.svg.png";
+    // console.log(event.target.name);
+    if (event.target.name === "gender") {
+        document.getElementById("inputValueContentGender").innerHTML = event.target.value;
+        if (event.target.value === "Female") {
+            document.getElementById("race").disabled = false;
+            document.getElementById("class").disabled = false;
+
+            document.getElementById("inputValueContentRace").innerHTML = document.getElementById("race").value;
+            document.getElementById("inputValueContentClass").innerHTML = document.getElementById("class").value;
+
+            // console.log("I work");
+            document.getElementById("gImg").src = "https://lexiq.hu/kepek/female.png";
+        } else if (event.target.value === "Male") {
+            document.getElementById("race").disabled = false;
+            document.getElementById("class").disabled = false;
+
+            document.getElementById("inputValueContentRace").innerHTML = document.getElementById("race").value;
+            document.getElementById("inputValueContentClass").innerHTML = document.getElementById("class").value;
+
+            document.getElementById("gImg").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Male-s%C3%ADmbolo2.svg/1200px-Male-s%C3%ADmbolo2.svg.png";
+        } else {
+            document.getElementById("race").disabled = true;
+            document.getElementById("class").disabled = true;
+
+            document.getElementById("inputValueContentRace").innerHTML = event.target.value;
+            document.getElementById("inputValueContentClass").innerHTML = event.target.value;
+
+            document.getElementById("gImg").src = "https://as1.ftcdn.net/v2/jpg/01/20/24/42/1000_F_120244259_3NWvUluAsywkvzuLtYKCNkj0jb8DgKpa.jpg";
+            document.getElementById("rImg").src = "https://as1.ftcdn.net/v2/jpg/01/20/24/42/1000_F_120244259_3NWvUluAsywkvzuLtYKCNkj0jb8DgKpa.jpg";
+            document.getElementById("cImg").src = "https://as1.ftcdn.net/v2/jpg/01/20/24/42/1000_F_120244259_3NWvUluAsywkvzuLtYKCNkj0jb8DgKpa.jpg";
+        }
+    } else if (event.target.name === "race") {
+        document.getElementById("inputValueContentRace").innerHTML = event.target.value;
+        if (event.target.value === "Dhampir") {
+            // console.log("I work");
+            document.getElementById("rImg").src = "https://images.squarespace-cdn.com/content/v1/5e3843beb37a0716cf512998/1618937697187-4SDCGSOWQK354XIYBO9P/dhampir.png";
+        } else if (event.target.value === "Dragonborn") {
+            document.getElementById("rImg").src = "https://cdn.shopify.com/s/files/1/0097/7810/2350/products/db01_cbf11502-48e2-473d-ae50-c7b477f60650_300x.png?v=1615259206";
+        } else if (event.target.value === "Dwarf") {
+            document.getElementById("rImg").src = "https://assets.3dtotal.com/gimli-final-maincam.5ddkbc.expanded.hpu.jpg";
+        } else {
+            document.getElementById("rImg").src = "https://image.shutterstock.com/image-vector/little-elf-vector-illustration-on-260nw-731890309.jpg";
+        }
     } else {
-        document.getElementById("gImg").src = "https://as1.ftcdn.net/v2/jpg/01/20/24/42/1000_F_120244259_3NWvUluAsywkvzuLtYKCNkj0jb8DgKpa.jpg";
+        document.getElementById("inputValueContentClass").innerHTML = event.target.value;
+        if (event.target.value === "Fighter") {
+            // console.log("I work");
+            document.getElementById("cImg").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgjlR5nVllPfQUR3M_AFT0x0JC02jPSG0Bhg&usqp=CAU";
+        } else if (event.target.value === "Programmer") {
+            document.getElementById("cImg").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVgANWFNZitlXr8ANBBXM0raIn0kV8KFl4g&usqp=CAU";
+        } else if (event.target.value === "Strider") {
+            document.getElementById("cImg").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROPqicDpJFRvkYnEJCnRZzX976MIyUjeSROw&usqp=CAU";
+        } else {
+            document.getElementById("cImg").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9UTleJ2eXbzsGkUF2fgy-uLRu0CvDykW5PQ&usqp=CAU";
+        }
     }
 }
 
